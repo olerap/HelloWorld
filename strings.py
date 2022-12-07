2a6f2144356ac9912a15635a949c5d671ead2756
Ole Rap <fn@olerap.nl>
2022-12-07 23:03:18 +0100

    did some more exercises

--- strings.py
+++ strings.py
@@ -4,9 +4,19 @@ print("Python's strings are easy to use")
 print('We can even include "quotes" in strings')
 print("hello" + " world")
 greeting = "Hello"
-name = input("Please enter your name ")
+name = "Ole"
 print(greeting + name)
 
 # if we want a space, we can add that too
 print(greeting + ' ' + name)
 
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age_in_words = "2 years"
+print(name + " is " + age + " years old")
+print(type(age))
